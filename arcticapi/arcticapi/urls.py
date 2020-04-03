1a639b45851c59bba0173350371dd2c009fd0957
Jeffrey Mohler <jeffreydmohler@outlook.com>
2020-04-03 13:26:52 -0600

    typing in url works

--- arcticapi/arcticapi/urls.py
+++ arcticapi/arcticapi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('client.urls')),
+    re_path(r'^.*/', include('client.urls')),
+    re_path(r'', include('client.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
